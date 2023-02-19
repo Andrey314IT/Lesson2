6e40da5225f836527ac89ed494516155e45a54db
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-02-19 09:26:18 +0000

    Added get older and wasting leaves

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 class Tree:
   leaves = 193829812931
   height = 10
+  age = 10
 
+  def __init__(self):
+    print("")
+  def get_older(self):
+    self.age += 1
+    self.leaves -= 10
 
 my_tree = Tree()
+print(my_tree.age, my_tree.leaves)
+my_tree.get_older()
+print(my_tree.age, my_tree.leaves)
