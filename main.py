65eb649f039de6d28ccc26d8563755f1b0c8cf52
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-02-19 09:39:08 +0000

    Added type of tree

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ class Tree:
   height = 10
   age = 10
 
-  def __init__(self):
-    print("")
+  def __init__(self, type):
+    self.type = type
   def get_older(self):
     self.age += 1
     self.leaves -= 10
     self.height += 10
 
-my_tree = Tree()
+my_tree = Tree("Akacia")
 print(my_tree.age, my_tree.leaves, my_tree.height)
+print(my_tree.type)
 my_tree.get_older()
 print(my_tree.age, my_tree.leaves, my_tree.height)
